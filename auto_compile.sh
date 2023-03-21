79835ceacb2e3730065c9698e9d47c64d0e0ea76
Sean Butler <seanxbutler@gmail.com>
2023-03-21 11:56:22 -0700

    fix auto build script and cmake to build the application with the correct pytorch build settings depending on wanting to use the debug or release versions of pytorch.
    also updated main.cpp and the include directive to reflect the change

--- auto_compile.sh
+++ auto_compile.sh
@@ -2,11 +2,25 @@
 
 # setup input and defaults
 
-build_type=$1
+if [[ "$MSYSTEM" != "MINGW64" ]]; then
+	echo "Need MSYS2 (MINGW64) to run this script. Windows powershell script in the works."
+	exit 1
+fi
 
-if [[ !build_type ]]; then
+build_type=$1
+pytorch_build_type=$2
+
+if [[ -z $build_type ]]; then
 	build_type="debug"
 fi
+
+echo "build type: $build_type"
+
+if [[ -z $pytorch_build_type ]]; then
+	pytorch_build_type="release"
+fi
+
+echo "build type: $pytorch_build_type"
 
 # create build folder
 
@@ -18,13 +32,29 @@ cd build
 
 # generate amd build
 
-cmake -G "Visual Studio 17 2022" ..
+if [ "$pytorch_build_type" == "debug" ]; then
+	cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug ..
+else
+	cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release ..
+fi
+
+if [ $? != 0 ]; then
+    cmake_version=$(cmake --version)
+    if [ -z $cmake_version ]; then
+		pacman -S mingw-w64-x86_64-cmake
+	else
+		echo "cmake command error!"
+	fi
+fi
+
 cmake --build . --config $build_type
 
 # copy needed dll files
 
-if [[ $build_type=="debug" ]]; then
-	cp ../libs/pytorch_1_13_0_$build_type/libtorch/lib/*.dll Debug/
+if [ "$build_type" == "debug" ]; then
+	echo "copy needed dlls for Debug folder"
+	cp ../libs/pytorch_1_13_0_$pytorch_build_type/libtorch/lib/*.dll Debug/
 else
-	cp ../libs/pytorch_1_13_0_$build_type/libtorch/lib/*.dll Release/
+	echo "copy needed dlls for Release folder"
+	cp ../libs/pytorch_1_13_0_$pytorch_build_type/libtorch/lib/*.dll Release/
 fi
