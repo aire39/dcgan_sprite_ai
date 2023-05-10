7b62ce0924839b74d6bd1835022f074b975e23d3
Sean Butler <seanxbutler@gmail.com>
2023-05-10 15:31:52 -0700

    add generator and discriminator classes

---
+++ capp/GeneratorImpl.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+#include <torch/torch.h>
+
+class GeneratorImpl : torch::nn::Module {
+  public:
+    GeneratorImpl(int64_t noise_size);
+    torch::Tensor forward(torch::Tensor x);
+
+  private:
+    std::array<torch::nn::ConvTranspose2d, 4> convolutions;
+    std::array<torch::nn::BatchNorm2d, 3> batchNormalizations;
+};
+
+TORCH_MODULE(Generator);
